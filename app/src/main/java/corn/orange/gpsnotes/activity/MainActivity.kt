c6b155bcab160b034cf33945700b9768d10b3429
Hozalex <hozalex@gmail.com>
2018-08-24 16:00:11 +0300

    cosmetics code

--- app/src/main/java/corn/orange/gpsnotes/activity/MainActivity.kt
+++ app/src/main/java/corn/orange/gpsnotes/activity/MainActivity.kt
@@ -12,5 +12,9 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
--- app/src/main/java/corn/orange/gpsnotes/adapter/NoteAdapter.kt
+++ app/src/main/java/corn/orange/gpsnotes/adapter/NoteAdapter.kt
@@ -1,5 +1,30 @@
 package corn.orange.gpsnotes.adapter
 
-class NoteAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>()
 {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder
+    {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int
+    {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int)
+    {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    {
+
+
+    }
 }
