efd4b239988e56698303265c9fbc2bf562fa441b
Hozalex <hozalex@gmail.com>
2018-08-28 16:04:52 +0300

    add changes

--- app/src/main/java/corn/orange/gpsnotes/activity/MainActivity.kt
+++ app/src/main/java/corn/orange/gpsnotes/activity/MainActivity.kt
@@ -1,20 +1,48 @@
 package corn.orange.gpsnotes.activity
 
-import android.app.ListActivity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.LinearLayoutManager
 import corn.orange.gpsnotes.R
+import corn.orange.gpsnotes.adapter.NoteAdapter
+import corn.orange.gpsnotes.adapter.NoteAdapter.OnNoteClickListener
+import corn.orange.gpsnotes.model.Note
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.item_note.*
 
 class MainActivity : AppCompatActivity()
 {
+
+    private lateinit var notesArrayList: ArrayList<Note>
+    private lateinit var note: Note
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        rvNotes.setHasFixedSize(true)
+        rvNotes.layoutManager = LinearLayoutManager(this)
+        notesArrayList = ArrayList(10)
+        setSupportActionBar(toolbar)
+        initActionBar()
+        rvNotes.adapter = NoteAdapter(notesArrayList, object : OnNoteClickListener
+        {
+            override fun onNoteClick(notePosition: Int)
+            {
+                tvNote.editableText
+                tvNoteDescription.editableText
+            }
+        })
 
+    }
 
-
-
+    private fun initActionBar()
+    {
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.open, R.string.close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
--- app/src/main/java/corn/orange/gpsnotes/adapter/NoteAdapter.kt
+++ app/src/main/java/corn/orange/gpsnotes/adapter/NoteAdapter.kt
@@ -1,30 +1,49 @@
 package corn.orange.gpsnotes.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import corn.orange.gpsnotes.R
+import corn.orange.gpsnotes.model.Note
+import kotlinx.android.synthetic.main.item_note.view.*
 
-class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>()
+class NoteAdapter(private val notesArrayList: ArrayList<Note>, private var onNoteClickListener: OnNoteClickListener) : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>()
 {
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder
     {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val viewHolder = NoteViewHolder(LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_note, parent, false))
+        viewHolder.itemView.setOnClickListener {
+            onNoteClickListener.onNoteClick(viewHolder.adapterPosition)
+        }
+        return viewHolder
     }
 
     override fun getItemCount(): Int
     {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return notesArrayList.size
     }
 
-    override fun onBindViewHolder(holder: NoteViewHolder, position: Int)
+    override fun onBindViewHolder(viewHolder: NoteViewHolder, position: Int)
     {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        viewHolder.tvNoteDescription.text = (notesArrayList[position].title).toUpperCase()
+        viewHolder.tvNote.text = (notesArrayList[position].content)
     }
 
 
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
+        val tvNote = itemView.tvNote!!
+        val tvNoteDescription = itemView.tvNoteDescription!!
+    }
 
+
+    interface OnNoteClickListener
+    {
+        fun onNoteClick(notePosition: Int)
 
     }
 }
