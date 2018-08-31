2fb2ce679370067932ab85b69877befaa1b7edb4
Hozalex <hozalex@gmail.com>
2018-08-31 09:52:10 +0300

    add App Bar

--- app/src/main/java/corn/orange/gpsnotes/activity/MainActivity.kt
+++ app/src/main/java/corn/orange/gpsnotes/activity/MainActivity.kt
@@ -1,23 +1,17 @@
 package corn.orange.gpsnotes.activity
 
-import android.app.Activity
 import android.os.Bundle
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.widget.LinearLayoutManager
-import corn.orange.gpsnotes.R
-import corn.orange.gpsnotes.adapter.NoteAdapter
-import corn.orange.gpsnotes.adapter.NoteAdapter.OnNoteClickListener
-import corn.orange.gpsnotes.model.Note
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar.*
-import kotlinx.android.synthetic.main.content_main.*
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import corn.orange.gpsnotes.R
 import corn.orange.gpsnotes.R.id.menu_add_note
 import corn.orange.gpsnotes.R.id.menu_clear_note
+import corn.orange.gpsnotes.model.Note
+import kotlinx.android.synthetic.main.app_bar.*
 
-class MainActivity : Activity()
+class MainActivity : AppCompatActivity()
 {
 
     private lateinit var notesArrayList: ArrayList<Note>
@@ -28,24 +22,17 @@ class MainActivity : Activity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         notesArrayList = ArrayList(10)
-//        setSupportActionBar(toolbar)
-        initActionBar()
-        rvNotes.adapter = NoteAdapter(notesArrayList, object : OnNoteClickListener
-        {
-            override fun onNoteClick(notePosition: Int)
-            {
-            }
-        })
-        rvNotes.layoutManager = LinearLayoutManager(rvNotes.context)
-        rvNotes.setHasFixedSize(true)
 
-    }
+        setSupportActionBar(my_toolbar)
+//        rvNotes.adapter = NoteAdapter(notesArrayList, object : OnNoteClickListener
+//        {
+//            override fun onNoteClick(notePosition: Int)
+//            {
+//            }
+//        })
+//        rvNotes.layoutManager = LinearLayoutManager(rvNotes.context)
+//        rvNotes.setHasFixedSize(true)
 
-    private fun initActionBar()
-    {
-        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.open, R.string.close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean
